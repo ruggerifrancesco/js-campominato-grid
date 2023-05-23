83de9715e656aa529309b98bf62b2c8ea991d85e
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-23 17:26:37 +0200

    moved the operation to new game into a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,28 +16,30 @@ window.addEventListener('load', () => {
   
 // Start New Game
 const startActualGameBtn = document.getElementById('play-btn');
+const gameGridContainer = document.getElementById('grid-game-container');
 
 startActualGameBtn.addEventListener ('click',
     function () {
-      
-      const gameGridContainer = document.getElementById('grid-game-container');
-
-      for (let i = 1; i <= 100; i++) {
-        const gameCell = createElement('div', 'bomb-cell');
-        const cellNumber = createElement('span', 'cell-number');
-        cellNumber.textContent = i; // Set the cell number
-
-        gameCell.addEventListener('click',
-          function () {
-            gameCell.classList.toggle('cell-selected');
-          }
-        )
-        
-        gameCell.appendChild(cellNumber); // Append the number element to the cell
-        gameGridContainer.appendChild(gameCell);
-      }
+      generateGrid();
     }
 )
+
+// Function to generate the grid
+function generateGrid() {
+
+  for (let i = 1; i <= 100; i++) {
+    const gameCell = createElement('div', 'bomb-cell');
+    const cellNumber = createElement('span', 'cell-number');
+    cellNumber.textContent = i; // Set the cell number
+
+    gameCell.addEventListener('click', function () {
+      gameCell.classList.toggle('cell-selected');
+    });
+
+    gameCell.appendChild(cellNumber); // Append the number element to the cell
+    gameGridContainer.appendChild(gameCell);
+  }
+}
 
 // Function to create new Element
 function createElement (tagName, className) {
